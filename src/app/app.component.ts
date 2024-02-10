5def967045632fdc76bbaaf17bffb77254908ccc
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-02-10 11:46:44 +0100

    step2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";//dodawanie komponentu
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HomeComponent]
+    imports: [CommonModule, RouterOutlet, HomeComponent,HeaderComponent,FooterComponent]
 })
 export class AppComponent {
   title = 'rent-a-pet-client';
