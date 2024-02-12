b15b5431da5bbe82df7b6c6f12a7ed60ba55a4c1
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-02-12 14:08:21 +0100

    another pets added

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -59,7 +59,78 @@ console.log("PETS CONSTRUCTOR")
         breed: "Border Collie",
         pricePerDay: 160.0,
         imageName: "border_collie.jpeg",
+      },
+
+
+      {
+        id: 5,
+        name: "Schroedinger",
+        type: "cat",
+        breed: "American Shorthair",
+        pricePerDay: 90.0,
+        imageName: "american_shorthair_cat.jpeg",
+      },
+
+      {
+        id: 6,
+        name: "Garfield",
+        type: "cat",
+        breed: "Orange Tabby",
+        pricePerDay: 110.0,
+        imageName: "orange_tabby_cat.jpeg",
+      },
+
+      {
+        id: 7,
+        name: "Laura",
+        type: "cat",
+        breed: "Scottish Fold",
+        pricePerDay: 100.0,
+        imageName: "scottish_fold_cat.jpeg",
+      },
+      {
+        id: 8,
+        name: "Gandalf",
+        type: "cat",
+        breed: "European Shorthair",
+        pricePerDay: 140.0,
+        imageName: "european_cat.jpeg",
+      },
+      {
+        id: 9,
+        name: "Zbigniew",
+        type: "exotic",
+        breed: "Pangular",
+        pricePerDay: 42.0,
+        imageName: "pangular.jpeg",
+      },
+      {
+        id: 10,
+        name: "Linus",
+        type: "exotic",
+        breed: "Linux",
+        pricePerDay: 210.0,
+        imageName: "penguin.jpeg",
+      },
+      {
+        id:11,
+        name: "Rider",
+        type: "exotic",
+        breed: "Beaver",
+        pricePerDay: 42.0,
+        imageName: "beaver.jpeg",
+      },
+      {
+        id: 12,
+        name: "Guido",
+        type: "exotic",
+        breed: "Python",
+        pricePerDay: 42.0,
+        imageName: "python.jpeg",
       }
+
+
+
 
 
     ] };
@@ -69,17 +140,10 @@ console.log("PETS CONSTRUCTOR")
       return '/assets/images/' + pet.imageName;
     }
   
-    // getFilteredPets() {
-    //   return this.filter === ''
-    //     ? this.pets
-    //     : this.pets.filter((product: any) => pet.type === this.filter);
-    // }
-
-
-    getPets(){
-
-      console.log("GETTING PETS");
-
-      return this.pets;
+    getFilteredPets() {
+      return this.filter === ''
+        ? this.pets
+        : this.pets.filter((pet: any) => pet.type === this.filter);
     }
+
 }
